5fee49beb9bdf885257e752d01e38e082651abf0
saad raja <shraja7@gmail.com>
2023-07-05 13:37:01 -0500

    backend repo

---
+++ README.md
@@ -0,0 +1,44 @@
+# Phonebook Backend
+
+This is a simple backend application for a phonebook. It provides endpoints to manage contacts such as retrieving all contacts, adding a contact, getting a single contact by ID, and deleting a contact.
+
+## Technologies Used
+
+- Node.js
+- Express.js
+- CORS (Cross-Origin Resource Sharing)
+- Morgan (HTTP request logger middleware)
+
+## Setup
+
+1. Clone the repository.
+2. Install the dependencies by running `npm install`.
+3. Start the server with `npm start`.
+
+## API Endpoints
+
+- `GET /api/persons`: Retrieves all contacts in the phonebook.
+- `GET /api/persons/:id`: Retrieves a single contact by ID.
+- `POST /api/persons`: Adds a new contact to the phonebook.
+- `DELETE /api/persons/:id`: Deletes a contact by ID.
+- `GET /info`: Provides information about the phonebook, including the number of contacts and the current date.
+
+## Data Structure
+
+The contacts are stored in an array called `persons`. Each contact has the following properties:
+
+- `id`: A unique identifier for the contact.
+- `name`: The name of the person.
+- `number`: The phone number of the person.
+
+## Logging
+
+The application uses the Morgan middleware to log information about each HTTP request. The logged information includes the request method, URL, response status, response time, and request body (if applicable).
+
+## Error Handling
+
+The application includes basic error handling for adding new contacts. It checks for missing name or number fields and ensures that each contact's name is unique. If an error occurs, the server responds with an appropriate error status code and a JSON error message.
+
+## License
+
+This project is licensed under the [MIT License](https://opensource.org/licenses/MIT).
---
+++ index.js
@@ -0,0 +1,99 @@
+//setup express server
+const express = require("express");
+const app = express();
+const cors = require("cors");
+
+//implement morgan
+const morgan = require("morgan");
+morgan.token("body", (req, res) => JSON.stringify(req.body));
+app.use(
+  morgan(":method :url :status :res[content-length] - :response-time ms :body")
+);
+
+//middleware
+app.use(cors());
+app.use(express.json());
+app.use(express.static("build"));
+
+let persons = [
+  {
+    id: 1,
+    name: "Arto Hellas",
+    number: "040-123456",
+  },
+  {
+    id: 2,
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+  },
+  {
+    id: 3,
+    name: "Dan Abramov",
+    number: "12-43-234345",
+  },
+  {
+    id: 4,
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+  },
+];
+//static files
+app.get("/api/persons", (req, res) => {
+  res.send(persons);
+});
+
+//info page
+app.get("/info", (req, res) => {
+  const date = new Date();
+  res.send(`<p>Phonebook has info for ${persons.length} people</p>
+    <p>${date}</p>`);
+});
+
+//route to get a single contact
+app.get("/api/persons/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const person = persons.find((person) => person.id === id);
+  if (person) {
+    res.json(person);
+  } else {
+    res.status(404).end();
+  }
+});
+
+//route to delete a contact
+app.delete("/api/persons/:id", (req, res) => {
+  const id = Number(req.params.id);
+  persons = persons.filter((person) => person.id !== id);
+  res.status(204).end();
+});
+
+//route to add a contact
+app.post("/api/persons", (req, res) => {
+  const body = req.body;
+  if (!body.name || !body.number) {
+    return res.status(400).json({
+      error: "name or number missing",
+    });
+  }
+  const nameExists = persons.find(
+    (p) => p.name.trim().toLowerCase() === body.name.trim().toLowerCase()
+  );
+  if (nameExists) {
+    return res.status(400).json({
+      error: "name must be unique",
+    });
+  }
+  const person = {
+    id: Math.floor(Math.random() * 1000),
+    name: body.name,
+    number: body.number,
+  };
+  persons = persons.concat(person);
+  res.json(person);
+});
+
+//listen to port
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
